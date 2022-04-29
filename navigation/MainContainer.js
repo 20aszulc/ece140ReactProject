d2871baf1318912590f2cec0210554f56efea347
aszulcucsd <33702922+20aszulc@users.noreply.github.com>
2022-04-28 17:31:34 -0700

    react code

---
+++ navigation/MainContainer.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+import {NavigationCon}
+
+
+export default function MainContainer(){
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
